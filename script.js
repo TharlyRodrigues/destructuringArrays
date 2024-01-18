af0a7f7306ce17a4fc31f4e25d80609a5107022d
TharlyRodrigues <tharly.portifolio@gmail.com>
2024-01-18 10:02:35 -0300

    update

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 'use strict';
-
+/*
 // Data needed for a later exercise
 // const flights =
 //   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
@@ -56,3 +56,14 @@ console.log(main, secondary);
 
 const [starter, mainCouser] = restaurant.order(2, 0);
 console.log(starter, mainCouser);
+
+// Nested destructuring
+const nested = [2, 4, [5, 6]];
+// const [i, , j] = nested;
+const [i, , [j, k]] = nested;
+console.log(i, j, k);
+
+// Default values
+const [p = 1, q = 1, r = 1] = [8, 5];
+console.log(p, q, r);
+*/

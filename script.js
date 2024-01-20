b7d524d208152c6b798794484662e19edeaa2db4
TharlyRodrigues <tharly.portifolio@gmail.com>
2024-01-20 20:37:02 -0300

    Logical Assignment Operators

--- script.js
+++ script.js
@@ -54,6 +54,38 @@ const restaurant = {
 };
 
 /*
+
+// Logical Assignment Operators
+const rest1 = {
+  name: 'capri',
+  // numGuests: 20,
+  numGuests: 0,
+};
+
+const rest2 = {
+  name: 'La pizza',
+  owner: 'giovanni Rossi',
+};
+
+// Or assignment operator
+// rest1.numGuests = rest1.numGuests || 10;
+// rest2.numGuests = rest2.numGuests || 10;
+
+// nullish assignment operator(null or undefined)
+rest1.numGuests ??= 10;
+rest2.numGuests ??= 10;
+
+// rest1.numGuests ||= 10;
+// rest2.numGuests ||= 10;
+
+// rest2.owner = rest2.owner && '<ANONYMOUS>';
+
+rest1.owner &&= '<ANONYMOUS>';
+rest2.owner &&= '<ANONYMOUS>';
+
+console.log(rest1);
+console.log(rest2);
+
 restaurant.nunGuests = 0;
 const guests = restaurant.nunGuests || 10;
 console.log(guests);

fd2f5ecdee70518c2e71adbe39a1f8f1b51297a8
TharlyRodrigues <tharly.portifolio@gmail.com>
2024-01-22 18:39:43 -0300

    Optional Chaining (?.)

--- script.js
+++ script.js
@@ -52,7 +52,30 @@ const restaurant = {
   },
 };
 
+if (restaurant.hours && restaurant.hours.mon)
+  console.log(restaurant.hours.mon.open);
+
+console.log(restaurant.hours.mon?.open);
+
+// exemple
+const days = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+for (const day of days) {
+  const open = restaurant.hours[day]?.open ?? 'closed';
+  console.log(`on ${day}, we open at ${open}`);
+}
+
+// methods
+console.log(restaurant.order?.(0, 1) ?? 'method does not exist');
+
+console.log(restaurant.orderRisotto?.(0, 1) ?? 'method does not exist');
+
+const user = [{ name: 'tharly', email: 'tharly@gmail.com' }];
+
+console.log(user[0]?.name ?? 'user array empty');
+
 /*
+///////////////////////////////////////////////
+// the for-of loop
 const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
 
 for (const item of menu) console.log(item);

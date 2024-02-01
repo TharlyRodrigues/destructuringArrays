3cb15f016c596bab1e45732511ede59e12258ebf
TharlyRodrigues <tharly.portifolio@gmail.com>
2024-01-31 23:02:39 -0300

    exercicio 4

--- exe.js
+++ exe.js
@@ -264,4 +264,19 @@ GOOD LUCK 😀
 */
 document.body.append(document.createElement('textarea'));
 document.body.append(document.createElement('button'));
-const text = document.querySelector('textarea').value;
+
+document.querySelector('button').addEventListener('click', function () {
+  const text = document.querySelector('textarea').value;
+  const rows = text.split('\n');
+  console.log(rows);
+  for (const row of rows) {
+    const [first, second] = row.toLocaleLowerCase().trim().split('_');
+    console.log(row, first, second);
+
+    const output = `${first}${second.replace(
+      second[0],
+      second[0].toUpperCase()
+    )}`;
+    console.log(output);
+  }
+});
